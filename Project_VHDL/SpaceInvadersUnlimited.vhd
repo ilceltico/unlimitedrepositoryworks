e9b3d95bd6aa9983cf3fa75a54680f99e9d28309
Kevin Selvaggi <kex812@hotmail.it>
2018-03-08 19:25:15 +0100

    Created VHDL project

---
+++ Project_VHDL/SpaceInvadersUnlimited.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity SpaceInvadersUnlimited is
+		Port 
+		( 
+				CLOCK : in  std_logic;
+				RESET : in  std_logic
+		);
+end entity;
+
+architecture RTL of SpaceInvadersUnlimited is
+	signal clock              : std_logic;
+
+begin
+end architecture;
