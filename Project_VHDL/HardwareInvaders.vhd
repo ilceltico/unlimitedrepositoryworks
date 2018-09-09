a732698c31aa15ef268bbf354e39264abe0b3d2e
kselvaggi <kevin.selvaggi@gmail.com>
2018-09-09 23:47:50 +0200

    Started implementing audio

--- Project_VHDL/HardwareInvaders.vhd
+++ Project_VHDL/HardwareInvaders.vhd
@@ -33,7 +33,30 @@ entity HardwareInvaders is
 		HEX0					  : out std_logic_vector(6 downto 0);
 		HEX1					  : out std_logic_vector(6 downto 0);
 		HEX2					  : out std_logic_vector(6 downto 0);
-		HEX3					  : out std_logic_vector(6 downto 0)
+		HEX3					  : out std_logic_vector(6 downto 0);
+		
+		-- WM8731 audio interface
+		AUD_BCLK				  : out std_logic;
+		AUD_XCK				  : out std_logic;
+		AUD_ADCLRCK			  :  out std_logic;
+		AUD_ADCDAT			  : in std_logic;
+		AUD_DACLRCK			  : out std_logic;
+		AUD_DACDAT			  : out std_logic;
+		
+		-- i2c
+		FPGA_I2C_SCLK		  : out std_logic;
+		FPGA_I2C_SDAT		  : inout std_logic;
+		
+		-- memory
+		onchip_memory2_0_s1_address     : in  std_logic_vector(17 downto 0) := (others => 'X'); -- address
+		onchip_memory2_0_s1_debugaccess : in  std_logic                     := 'X';             -- debugaccess
+		onchip_memory2_0_s1_clken       : in  std_logic                     := 'X';             -- clken
+		onchip_memory2_0_s1_chipselect  : in  std_logic                     := 'X';             -- chipselect
+		onchip_memory2_0_s1_write       : in  std_logic                     := 'X';             -- write
+		onchip_memory2_0_s1_readdata    : out std_logic_vector(15 downto 0);                    -- readdata
+		onchip_memory2_0_s1_writedata   : in  std_logic_vector(15 downto 0) := (others => 'X'); -- writedata
+		onchip_memory2_0_s1_byteenable  : in  std_logic_vector(1 downto 0)  := (others => 'X'); -- byteenable
+		onchip_memory2_0_reset1_reset   : in  std_logic                     := 'X'              -- reset	
 	);
 end entity;
 
@@ -104,6 +127,7 @@ architecture RTL of HardwareInvaders is
 	signal rand_output							: std_logic_vector(RAND_ALIEN_GENERATION_TIME_BITS-1 downto 0);
 	
 	-- Clocks
+	signal clock_12MHz        					: std_logic;
 	signal clock_50MHz        					: std_logic;
 	signal clock_debug		  					: std_logic;
 	signal clock_100MHz       					: std_logic;
@@ -142,7 +166,18 @@ architecture RTL of HardwareInvaders is
 	signal show_next_level						: std_logic;
 	signal show_intro								: std_logic;
 	signal restart_game							: std_logic;
-
+	
+	-- audio signals
+	signal bitprsc									: integer range 0 to 4:=0;
+	signal aud_mono								: std_logic_vector(31 downto 0):=(others=>'0');
+	signal read_addr								: integer range 0 to 240254:=0;
+	signal ROM_ADDR								: std_logic_vector(17 downto 0);
+	signal ROM_OUT									: std_logic_vector(15 downto 0);
+	signal WM_i2c_busy							: std_logic;
+	signal WM_i2c_done							: std_logic;
+	signal WM_i2c_send_flag						: std_logic;
+	signal WM_i2c_data							: std_logic_vector(15 downto 0);
+	signal DA_CLR									: std_logic:='0';
 	
 begin
 
@@ -150,7 +185,8 @@ begin
 		port map (
 			inclk0  => CLOCK_50,
 			c0      => clock_100MHz,
-			c1      => clock_50MHz
+			c1      => clock_50MHz,
+		-- c2		  => clock_12MHz
 		); 
 	
 					
@@ -228,6 +264,7 @@ begin
 	end process;
 	
 	VGA_VS <= fb_vsync;
+	
 	
 	vga : entity work.VGA_Framebuffer
 		generic map (
@@ -597,5 +634,120 @@ begin
 			end if;
 			
 		end process;
+		
+	-- AUDIO
+	sound : entity work.aud_gen 
+		port map(
+			aud_clock_12	=>	clock_12MHz,
+			aud_bk			=>	AUD_BCLK,
+			aud_dalr			=>	DA_CLR,
+			aud_dadat		=>	AUD_DACDAT,	
+			aud_data_in		=>	aud_mono
+		);
+
+	WM8731: entity work.i2c 
+		port map(
+			i2c_busy			=>	WM_i2c_busy,
+			i2c_scl			=>	FPGA_I2C_SCLK,
+			i2c_send_flag	=>	WM_i2c_send_flag,
+			i2c_sda			=>	FPGA_I2C_SDAT,
+			i2c_addr			=>	"00110100",
+			i2c_done			=>	WM_i2c_done,
+			i2c_data			=>	WM_i2c_data,
+			i2c_clock_50	=>	clock_50	
+		);
+		
+	AUD_XCK			<=	clock_12MHz;
+	AUD_DACLRCK		<=	DA_CLR;
+		
+	ROM_ADDR			<=	std_logic_vector(to_unsigned(read_addr,18));
+	
+	handle_audio : process (clock_12MHz)
+	begin
+
+		if rising_edge (clock_12MHz) then
+
+			if(SW(8)='1')then--------reset
+				read_addr	<= 0;
+				bitprsc		<= 0;
+				aud_mono		<= (others=>'0');
+			else
+			LEDR(1)		<= SW(7);
+			aud_mono(15 downto 0) <= ROM_OUT; ----mono sound
+			aud_mono(31 downto 16) <= ROM_OUT;
+			  if (DA_CLR='1') then
+					if (bitprsc<5) then ----8ksps
+						bitprsc	<= bitprsc+1;
+					else
+						bitprsc<=0;
+						if (read_addr<240254) then
+							read_addr <= read_addr+1;
+						else
+							read_addr <= 0;
+						end if;
+					end if;
+				end if;
+			end if;
+
+		end if;
+
+	end process;	
+
+	handle_i2c : process (CLOCK_50)
+	begin
+
+		if rising_edge (CLOCK_50)then
+			if(KEY="1111")then
+			WM_i2c_send_flag<='0';
+			end if;
+		end if;
+		 if rising_edge(CLOCK_50) and WM_i2c_busy='0' then
+		 
+			
+				if (KEY(0)='0') then ----Digital Interface: DSP, 16 bit, slave mode
+				WM_i2c_data(15 downto 9)<="0000111";
+				WM_i2c_data(8 downto 0)<="000010011";	
+				WM_i2c_send_flag<='1';
+					
+				elsif (KEY(0)='0'AND SW(0)='1' ) then---HEADPHONE VOLUME
+				WM_i2c_data(15 downto 9)<="0000010";
+				WM_i2c_data(8 downto 0)<="101111001";
+				WM_i2c_send_flag<='1';
+				
+				elsif (KEY(1)='0'AND SW(0)='0' ) then---ADC of, DAC on, Linout ON, Power ON
+				WM_i2c_data(15 downto 9)<="0000110";
+				WM_i2c_data(8 downto 0)<="000000111";
+			
+				WM_i2c_send_flag<='1';
+				elsif (KEY(1)='0'AND SW(0)='1' ) then---USB mode
+				WM_i2c_data(15 downto 9)<="0001000";
+				WM_i2c_data(8 downto 0)<="000000001";
+				
+				WM_i2c_send_flag<='1';
+				elsif (KEY(2)='0'AND SW(0)='0') then---activ interface
+				WM_i2c_data(15 downto 9)<="0001001";
+				WM_i2c_data(8 downto 0)<="111111111";
+				
+				WM_i2c_send_flag<='1';
+				elsif (KEY(2)='0'AND SW(0)='1') then---Enable DAC to LINOUT
+				WM_i2c_data(15 downto 9)<="0000100";
+				WM_i2c_data(8 downto 0)<="000010010";
+				
+				WM_i2c_send_flag<='1';
+				elsif (KEY(3)='0' AND SW(0)='0') then---remove mute DAC
+				WM_i2c_data(15 downto 9)<="0000101";
+				WM_i2c_data(8 downto 0)<="000000000";
+				
+				WM_i2c_send_flag<='1';
+				elsif (KEY(3)='0' AND SW(0)='1') then---reset
+				WM_i2c_data(15 downto 9)<="0001111";
+				WM_i2c_data(8 downto 0)<="000000000";
+				
+				WM_i2c_send_flag<='1';
+				end if;
+
+		 end if;
+	end process;
+
 		
 end architecture;
